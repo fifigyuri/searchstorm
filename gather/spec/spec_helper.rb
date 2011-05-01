74e9f518e24ce7d32f9f793c2818124e52792f89
Georg Fruhwald <fifigyuri@gmail.com>
2011-05-01 12:30:52 +0200

    added guard with rspec and spork

--- gather/spec/spec_helper.rb
+++ gather/spec/spec_helper.rb
@@ -1,17 +1,17 @@
+require 'rubygems'
+require 'spork'
 
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../test_app/config/environment", __FILE__)
-require 'rspec/rails'
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../test_app/config/environment", __FILE__)
+  require 'rspec/rails'
+  RSpec.configure do |c|
+    c.mock_with :rspec
+  end
+end
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-
-# Load Factories:
-Dir[File.join(File.dirname(__FILE__), "factories/**/*.rb")].each {|f| require f}
-
-Rails.application.scraping.url_seed = 'http://example.com'
-
-RSpec.configure do |c|
-  c.mock_with :rspec
+Spork.each_run do
+  Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+  Dir[File.join(File.dirname(__FILE__), "factories/**/*.rb")].each {|f| require f}
+  Rails.application.scraping.url_seed = 'http://example.com'
 end
